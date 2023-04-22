dc8cb9442edd0c3767a78ce6e1daa0b821b5eb99
DracoCS <67494021+Danial-Suhail@users.noreply.github.com>
2023-04-22 12:55:21 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+
+app.use(express.static('public'));
 
 app.listen(3000, function(){
     console.log("Server started on Port 3000");
 });
+
+
+
